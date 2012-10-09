b5246f7b748a88209ad069719d113c035f6138dc
x401om <x401om@gmail.com>
2012-10-10 00:01:33 +0400

    Project added

---
+++ StressIt/StressIt/NLMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLMainMenuViewController.h
+//  StressIt
+//
+//  Created by Alexey Goncharov on 09.10.12.
+//  Copyright (c) 2012 NIALSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLMainMenuViewController : UIViewController
+
+@end
---
+++ StressIt/StressIt/NLMainMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  NLMainMenuViewController.m
+//  StressIt
+//
+//  Created by Alexey Goncharov on 09.10.12.
+//  Copyright (c) 2012 NIALSoft. All rights reserved.
+//
+
+#import "NLMainMenuViewController.h"
+
+@interface NLMainMenuViewController ()
+
+@end
+
+@implementation NLMainMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
